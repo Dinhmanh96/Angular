c9f8c2b79e70b4c8f7237a45f9a3671fd3f80c28
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-08-16 22:42:36 +0700

    Update folder angular

---
+++ VT/index.php
@@ -0,0 +1,300 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>VT</title>
+	<!-- <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto"> -->
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick-theme.css"/>
+	<link rel="stylesheet/less" type="text/css" href="styles.less"/>
+	<link rel="stylesheet/less" type="text/css" href="media.less"/>
+	<!-- Less js -->
+	<script src="less/dist/less.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- <script src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script> -->
+	<!-- jQuery library -->
+	<!-- <script src="js/jquery-3.2.1.min.js"></script> -->
+
+	<!-- Latest compiled JavaScript -->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+</head>
+<body>
+	<div class="container">
+		
+		<hr>
+		<div class="row slide">
+			<div class="slide-item thumbnail">
+				<img src="images/slide1.jpg" alt="">
+			</div>
+			<div class="slide-item thumbnail">
+				<img src="images/slide2.jpg" alt="">
+			</div>
+			<div class="slide-item thumbnail">
+				<img src="images/slide1.jpg" alt="">
+			</div>
+			<div class="slide-item thumbnail">
+				<img src="images/slide2.jpg" alt="">
+			</div><div class="slide-item thumbnail">
+				<img src="images/slide1.jpg" alt="">
+			</div>
+			<div class="slide-item thumbnail">
+				<img src="images/slide2.jpg" alt="">
+			</div>
+		</div>
+
+		<div class="row intro">
+			<div class="col-sm-6 left-content">
+				<blockquote>
+					<h3>GIỚI THIỆU VỀ HỆ THỐNG</h3>
+				</blockquote>
+				<p class="title">Hệ thống hợp tác, quản lý hồ sơ - hợp đồng</p>
+				<p class="text">
+					<span>
+						Tổng Công ty Viễn thông Viettel (Viettel Telecom) - Chi nhánh Tập đoàn Viễn thông Quân đội. Chính thức gia nhập thị trường viễn thông từ ngày 15/10/2000, với mục tiêu trở thành nhà cung cấp dịch vụ viễn thông hàng đầu Việt Nam, Viettel Telecom luôn coi sự sáng tạo và tiên phong là kim chỉ nam hành động.
+					</span>
+					<br>
+					<span>
+						Đó không chỉ là sự tiên phong về mặt công nghệ mà còn là sự sáng tạo trong triết lý kinh doanh, thấu hiểu và thỏa mãn nhu cầu của khách hàng. Mọi ý kiến và thắc mắc về chương trình xin gửi về địa chỉ mail: cskh@viettel.com.vn hoặc gọi tổng đài 18008098 (miễn phí).
+					</span>
+					<br>
+					<span>
+						Viettel xin chân thành cảm ơn Quý khách.
+					</span>
+					<br>
+					<span>
+						Kính chúc quý khách mạnh khỏe, hạnh phúc và thành đạt!
+					</span>
+				</p>
+				<a href="#">
+					<p>Xem thêm...</p>
+				</a>
+			</div>
+			<div class="col-sm-6 right-content">
+				<img class="img-responsive img" src="images/laptop.jpg" alt="">
+			</div>
+		</div>
+		<hr>
+		<div class="row rule">
+			<blockquote>
+				<h3>QUY ĐỊNH - CHÍNH SÁCH</h3>
+			</blockquote>
+			<div class="col-md-4 left-content">
+				<img class="img-responsive" src="images/slide3.jpg" alt="">
+			</div>
+			<div class="col-md-8 right-content">
+				<div class="col-sm-4 rule-item">
+					<p class="title">Văn bản pháp luật</p>
+					<ul>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+					</ul>
+					<a href=""><p>Xem thêm...</p></a>
+				</div>
+				<div class="col-sm-4 rule-item">
+					<p class="title">Quy định của Viettel</p>
+					<ul>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+					</ul>
+					<a href=""><p>Xem thêm...</p></a>
+				</div>
+				<div class="col-sm-4 rule-item">
+					<p class="title fix-text">Thủ tục và chính sách hợp tác</p>
+					<ul>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+						<a href="#"><li>Chỉ thị 07/CT-BBCT</li></a>
+						<a href="#"><li>Thông tư 02/2007/TT-BBCVT</li></a>
+					</ul>
+					<a href=""><p>Xem thêm...</p></a>
+				</div>
+				<div class="col-sm-12 download">
+					<a href="#"><i class="fa fa-download" aria-hidden="true"></i><span>Tải Hồ sơ mẫu</span></a>
+					<a href="#"><i class="fa fa-download contract" aria-hidden="true"></i><span>Tải Hợp đồng mẫu</span></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="row banner-content">
+			<img class="img-responsive" src="images/banner2.jpg" alt="">
+		</div>
+		
+		<blockquote class="row quote">
+			<div class="col-sm-6">
+				<h3>TIN TỨC - SỰ KIỆN</h3>
+			</div>
+			<!-- <div class="clearfix"></div> -->
+			<div class="col-sm-6">
+				<a href="#"><span>Xem tất cả bài viết</span></a>
+			</div>
+		</blockquote>
+		<div class="row news">
+			<div class="news-item thumbnail">
+				<img src="images/slide4.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide5.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017.</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide6.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide4.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide5.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017.</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide6.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide4.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide5.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017.</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+			<div class="news-item thumbnail">
+				<img src="images/slide6.jpg" alt="">
+				<div class="caption">
+					<p class="title">Viettel Post miễn phí đổi sim 4G cho khách hàng đến 30/6/2017</p>
+					<p class="description">Từ hôm nay, các bưu cục của Viettel Post trên toàn quốc sẽ miễn phí đổi sim 4G cho khách hàng. Chương trình sẽ được triển khai đến hết ngày 30/06/2017.</p>
+					<a href="#"><p>Xem thêm...</p></a>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<div class="progress">
+		    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+		      40%
+		    </div>
+		  </div>
+	</div>
+
+
+	
+	<script type="text/javascript" src="slick-1.6.0/slick/slick.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.slide').slick({
+				dots: true,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 4000,
+				responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						infinite: true,
+						autoplay: true,
+						autoplaySpeed: 2000
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 2000
+					}
+				},
+				]
+			});
+			$('.news').slick({
+				dots: true,
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 4000,
+				responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						infinite: true,
+						autoplay: true,
+						autoplaySpeed: 2000
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 2000
+					}
+				},
+				]
+			});
+
+
+	  //     $('.panel .slick-next').on('click', function() {
+			// $('.pack-data').slick('slickNext')
+	  //     });
+
+	  //     $('.panel .slick-prev').on('click', function() {
+			// $('.pack-data').slick('slickPrev')
+	  //     });
+	});
+</script>
+</body>
+</html>
