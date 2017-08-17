274ddc28e60aec2ff7131730cac5dbfe4fbf8f4f
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-08-18 02:50:27 +0700

    Update 18/8/2017 2:50AM

---
+++ VT/Manh1/index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>VT</title>
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet/less" type="text/css" href="styles.less"/>
+	<link rel="stylesheet/less" type="text/css" href="media.less"/>
+	<!-- Less js -->
+	<script src="less/dist/less.js" type="text/javascript"></script>
+	<script src="js/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		
+
+
+	<div class="progress">
+		    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+		      40%
+		    </div>
+		  </div>
+	</div>
+	
+</body>
+</html>
